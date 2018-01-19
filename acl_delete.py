fe7dad3bddcd87c30c23520513c32ce0d1222e90
Igor JS <sanz@gta.ufrj.br>
2018-01-19 09:01:16 -0200

    File updates

--- acl_delete.py
+++
@@ -1,9 +0,0 @@
-import requests
-import sys
-
-# python acl_delete.py acl1 acl2 acl3 ...
-
-for acl_name in sys.argv[1:]:
-   acl_url = 'http://admin:admin@10.240.114.130:8181/restconf/config/ietf-access-control-list:access-lists/acl/ietf-access-control-list:ipv4-acl/' + acl_name
-   r = requests.delete(acl_url)
-   print acl_name, 'deleting status:', r.status_code
